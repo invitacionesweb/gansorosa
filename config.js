3a19be75beef98c8d3ea73b28090491e8e270aec
invitacionesweb <invitacionesweb.info@gmail.com>
2026-05-08 20:40:58 -0300

    ACTUALLIZAR
    AC

---
+++ config.js
@@ -0,0 +1,41 @@
+// =============================================
+//  CONFIGURACIÓN DE LA INVITACIÓN - ROMA
+//  Editá solo este archivo para cada cliente
+// =============================================
+
+const CONFIG = {
+
+  // --- DATOS DEL EVENTO ---
+  nombreFestejado: "Julia",
+  subtitulo: "Te esperamos",
+
+  // --- FECHA PARA EL CONTADOR (ISO 8601) ---
+  fechaContador: "2026-07-25T23:59:59",
+
+  // --- UBICACIÓN ---
+  textoUbicacion: "Te esperamos para disfrutar mi primer añito a partir de las 17:30hs - La Carmela",
+  linkMapa: "https://www.google.com/maps/dir//La+Carmela,+S+J+Bynnon+2615,+Jos++M+rmol,+Buenos+Aires,+Jorge+Santiago+Bynnon+2615,+Jos%C3%A9+Marmol,+Provincia+de+Buenos+Aires/@-34.7890687,-58.373926,17z/data=!4m17!1m7!3m6!1s0x95bcd3335dbb9655:0x5df722b3f2f483c2!2sLa+Carmela!8m2!3d-34.7890687!4d-58.373926!16s%2Fg%2F1q62k4h8g!4m8!1m0!1m5!1m1!1s0x95bcd3335dbb9655:0x5df722b3f2f483c2!2m2!1d-58.3739261!2d-34.7890685!3e0!18m1!1e1?entry=ttu&g_ep=EgoyMDI2MDUwNi4wIKXMDSoASAFQAw%3D%3D",
+  linkAgendar: "https://calendar.app.google/fSbvCLJugN3k9zHo6",
+
+  // --- FORMULARIO ---
+  textoForm: "Tu presencia es lo más importante",
+  textoFormSub: "NO OLVIDES CONFIRMAR ASITENCIA CON MIS PAPIS ANTES DEL 07 DE JULIO.",
+  maxInvitados: 5,
+  scriptFormUrl: "https://wa.link/yakt18",
+
+  // --- AUDIO ---
+  archivoAudio: "./ABCD.mp3",
+
+  // --- COLORES ---
+  colores: {
+    color0: "#ffffff",
+    color1: "#E4C7C5",
+    color2: "#f8f3ed",
+    color3: "#E4C7C5",
+  },
+
+  // --- TÍTULO DE LA PESTAÑA ---
+  tituloTab: "Julia",
+  whatsappNumero: "1140930654",  // sin + ni espacios
+whatsappMensaje: "🪿🎀 ¡Hola! Gracias por la invitación, ahí estaré/mos 🪿🎀",
+};
