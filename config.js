d11ed3a38faadf597ddb9d970eda2a6c296668a6
invitacionesweb <invitacionesweb.info@gmail.com>
2026-05-09 08:48:02 -0300

    aa
    aaa

--- config.js
+++ config.js
@@ -14,6 +14,8 @@ const CONFIG = {
 
   // --- UBICACIÓN ---
   textoUbicacion: "Te esperamos para disfrutar mi primer añito a partir de las 17:30hs - La Carmela",
+  textoUbicacion2: "Te xxxxxx para disfrutar mi primer añito a partir de las 17:30hs - La Carmela",
+
   linkMapa: "https://www.google.com/maps/dir//La+Carmela,+S+J+Bynnon+2615,+Jos++M+rmol,+Buenos+Aires,+Jorge+Santiago+Bynnon+2615,+Jos%C3%A9+Marmol,+Provincia+de+Buenos+Aires/@-34.7890687,-58.373926,17z/data=!4m17!1m7!3m6!1s0x95bcd3335dbb9655:0x5df722b3f2f483c2!2sLa+Carmela!8m2!3d-34.7890687!4d-58.373926!16s%2Fg%2F1q62k4h8g!4m8!1m0!1m5!1m1!1s0x95bcd3335dbb9655:0x5df722b3f2f483c2!2m2!1d-58.3739261!2d-34.7890685!3e0!18m1!1e1?entry=ttu&g_ep=EgoyMDI2MDUwNi4wIKXMDSoASAFQAw%3D%3D",
   linkAgendar: "https://calendar.app.google/fSbvCLJugN3k9zHo6",
 
